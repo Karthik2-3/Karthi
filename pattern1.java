4d567d84657ed4018568f197c9e2ce943e518841
Karthik2-3 <911karthick@gmail.com>
2023-12-10 20:47:13 +0530

    pattern increment program

---
+++ pattern1.java
@@ -0,0 +1,14 @@
+package Karthi;
+import java.util.Scanner;
+public class pattern1 {
+ public static void main(String[] args) {
+    Scanner io=new Scanner(System.in);
+    int value=io.nextInt();
+    for(int i=0;i<=value;i++){
+        for(int j=0;j<=i;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
+}
